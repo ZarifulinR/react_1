352da5b12d10a626a20dc2b08a811f50de06ae5a
ZarifulinR <fred-c112@yandex.ru>
2025-08-26 10:40:57 +0300

    Nav Done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,11 @@
 import logo from '../../logo.svg';
 import './Header.css';
 function Header() {
+    let title= "Introduction to React"
     return (
-        <heder className=' App-heder' >
+        <heder className= "App-heder" >
             <img src={logo} className="App-logo" alt="logo" />
+            <hi> {title} </hi>
             <p>
                 Edit <code>src/App.js</code> and save to reload.
             </p>
