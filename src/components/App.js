d396ce786c944298b453f33d4524b78410549de3
ZarifulinR <fred-c112@yandex.ru>
2025-08-26 08:51:17 +0300

    Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
-import Header from './Header.js';
-import Article from './Article.js';
-import Footer from './Footer.js';
+import Header from './header/Header.js';
+import Article from './article/Article.js';
+import Footer from './footer/Footer.js';
 
 function App() {
   return (
